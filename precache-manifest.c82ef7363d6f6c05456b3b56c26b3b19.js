55bdf38fbb104db34cbc5e0e0e50713137ee6718
Kawoa NMC <manhcuong0978481376@gmail.com>
2022-03-04 18:11:57 +0700

    Updates

---
+++ precache-manifest.c82ef7363d6f6c05456b3b56c26b3b19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfe073946b13a93a578196375f23ec0",
+    "url": "/react/photo-gallery/index.html"
+  },
+  {
+    "revision": "43244be237572b733e69",
+    "url": "/react/photo-gallery/static/css/main.e199c630.chunk.css"
+  },
+  {
+    "revision": "d49c8d834d4374208ada",
+    "url": "/react/photo-gallery/static/js/2.c598e55b.chunk.js"
+  },
+  {
+    "revision": "df846bc52d934438724c241c3efb95c9",
+    "url": "/react/photo-gallery/static/js/2.c598e55b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43244be237572b733e69",
+    "url": "/react/photo-gallery/static/js/main.01656e29.chunk.js"
+  },
+  {
+    "revision": "07fcd6a67298e1cb5170",
+    "url": "/react/photo-gallery/static/js/runtime-main.aa6f6d3c.js"
+  }
+]);
